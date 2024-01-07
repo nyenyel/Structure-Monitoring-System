002d48e71b89e8934144bec1a6fae8d938c87951
taba-taba <nieltayag10@gmail.com>
2024-01-08 02:53:44 +0800

    Sample file

--- app/src/main/java/com/example/structuremonitoringsystem/Sample.java
+++ app/src/main/java/com/example/structuremonitoringsystem/Sample.java
@@ -1,4 +1,10 @@
 package com.example.structuremonitoringsystem;
 
+import androidx.annotation.Nullable;
+
 public class Sample {
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        return super.equals(obj);
+    }
 }
