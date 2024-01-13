2950c843bc1af6988c4c07db9a40f09d69559698
taba-taba <nieltayag10@gmail.com>
2024-01-13 10:55:39 +0800

    added a getFunction for device MAC and Name

--- app/src/main/java/com/example/structuremonitoringsystem/MainActivity.java
+++ app/src/main/java/com/example/structuremonitoringsystem/MainActivity.java
@@ -8,18 +8,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.example.structuremonitoringsystem.Bluetooth.BluetoothConnection;
 import com.example.structuremonitoringsystem.OpenGL.OpenGLView;
+import com.example.structuremonitoringsystem.Variables.GlobalVariable;
 
 public class MainActivity extends AppCompatActivity {
 
     Context context;
     private OpenGLView openGLView;
+    private BluetoothConnection bluetoothConnection;
     private AppCompatButton upBtn, downBtn, leftBtn, rightBtn, centerBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        GlobalVariable globalVariable = new GlobalVariable();
 
         openGLView = (OpenGLView) findViewById(R.id.openGLView);
         upBtn = (AppCompatButton) findViewById(R.id.upBtn);
@@ -27,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         leftBtn = (AppCompatButton) findViewById(R.id.leftBtn);
         rightBtn = (AppCompatButton) findViewById(R.id.rightBtn);
         centerBtn = (AppCompatButton) findViewById(R.id.centerBtn);
+        context = this;
+
 
         upBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,9 +68,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openGLView.test();
-
+                String MAC = globalVariable.getDeviceMacList(context);
+                String name= globalVariable.getDeviceNameList(context);
+                Log.e("MAC", MAC+"");
+                Log.e("Name", name+"");
             }
         });
+
+
 
     }
 
