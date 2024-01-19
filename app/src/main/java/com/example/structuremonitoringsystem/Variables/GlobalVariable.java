cda3fe5948496c6872ece37652c2bf0fefef3f72
taba-taba <nieltayag10@gmail.com>
2024-01-20 05:07:58 +0800

    new function for merging arrays of float

--- app/src/main/java/com/example/structuremonitoringsystem/Variables/GlobalVariable.java
+++ app/src/main/java/com/example/structuremonitoringsystem/Variables/GlobalVariable.java
@@ -8,6 +8,35 @@ import com.example.structuremonitoringsystem.Bluetooth.BluetoothConnection;
 public class GlobalVariable {
 
     public String arduinoDeviceMAC = "";
+    public float width = 0f;
+    public float height = 0f;
+    public float thickness = 0f;
+
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getThickness() {
+        return thickness;
+    }
+
+    public void setThickness(float thickness) {
+        this.thickness = thickness;
+    }
+
 
     private BluetoothConnection bluetoothConnection;
 
@@ -24,6 +53,20 @@ public class GlobalVariable {
         return deviceNameList;
     }
 
+    public static String mergeFloatArrayToString(float[] floatArray, String delimiter) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < floatArray.length; i++) {
+            stringBuilder.append(floatArray[i]);
+
+            // Add the delimiter if it's not the last element
+            if (i < floatArray.length - 1) {
+                stringBuilder.append(delimiter);
+            }
+        }
+
+        return stringBuilder.toString();
+    }
 
 
 
