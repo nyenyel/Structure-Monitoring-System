880407f9b1e8bd7ef12f1f1e23b3e7c537db977c
taba-taba <nieltayag10@gmail.com>
2024-01-21 16:50:45 +0800

    Function for CRUD (Create, Read, Update, and Delete) local database.

---
+++ app/src/main/java/com/example/structuremonitoringsystem/LocalDatabase/DatabaseHelper.java
@@ -0,0 +1,104 @@
+package com.example.structuremonitoringsystem.LocalDatabase;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private Context context;
+
+    private static String DATABASE_NAME = "MonitoringSystem.db";
+
+    private static String TABLE_NAME = "object_template";
+    private static String COL_ID = "_id";
+    private static String COL_TEMPLATE_NAME = "template_name";
+    private static String COL_TEMPLATE_WIDTH = "_width";
+    private static String COL_TEMPLATE_HEIGHT = "_height";
+    private static String COL_TEMPLATE_THICKNESS = "_thickness";
+    public DatabaseHelper(Context context){
+        super(context, DATABASE_NAME, null,1);
+        this.context = context;
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE "+ TABLE_NAME
+                + " ("+ COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COL_TEMPLATE_NAME + " TEXT, "
+                + COL_TEMPLATE_HEIGHT + " REAL, "
+                + COL_TEMPLATE_WIDTH + " REAL, "
+                + COL_TEMPLATE_THICKNESS + " REAL);" ;
+        db.execSQL(query);
+
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+
+    public void addTemplate(String templateName, float width, float height, float thickness){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COL_TEMPLATE_NAME, templateName);
+        contentValues.put(COL_TEMPLATE_WIDTH, width);
+        contentValues.put(COL_TEMPLATE_HEIGHT, height);
+        contentValues.put(COL_TEMPLATE_THICKNESS, thickness);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1){
+            Log.e("Data Progress", "Data insertion Failed");
+        }else{ Log.e("Data Progress", "Data insertion Success");}
+    }
+
+    public void updateTemplateData(String id ,String templateName, float width, float height, float thickness){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COL_TEMPLATE_NAME, templateName);
+        contentValues.put(COL_TEMPLATE_WIDTH, width);
+        contentValues.put(COL_TEMPLATE_HEIGHT, height);
+        contentValues.put(COL_TEMPLATE_THICKNESS, thickness);
+
+        long result = db.update(TABLE_NAME, contentValues, COL_ID+"=?", new String[]{id});
+        if(result == -1){
+            Log.e("Data Progress", "Data update Failed");
+        }else{ Log.e("Data Progress", "Data update Success");}
+
+    }
+    public void deleteTemplate(String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        long result = db.delete(TABLE_NAME, COL_ID+"=?", new String[]{id});
+        if(result == -1){
+            Log.e("Data Progress", "Data Delete Failed");
+        }else{ Log.e("Data Progress", "Data Delete Success");}
+
+    }
+    public Cursor readAllData(){
+        String query = "SELECT * FROM "+ TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if(db != null){
+            cursor = db.rawQuery(query, null);
+        }
+        return cursor;
+    }
+
+    public Cursor getSpecificDataById(String id){
+        String query = "SELECT * FROM " + TABLE_NAME + " WHERE " + COL_ID + " = " + id;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if(db != null){
+            cursor = db.rawQuery(query, null);
+        }
+        return cursor;
+    }
+
+
+}
