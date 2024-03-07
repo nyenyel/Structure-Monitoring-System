d7fc773f06dc4e0d6e0fc7adb232f920ab3d5b01
nyenyel <nieltayag@gmail.com>
2024-03-07 15:50:26 +0800

    revised the design for other function testing, added a recycler view to display paired bluetooth devices.

--- app/src/main/java/com/example/structuremonitoringsystem/BluetoothTest.java
+++ app/src/main/java/com/example/structuremonitoringsystem/BluetoothTest.java
@@ -2,6 +2,8 @@ package com.example.structuremonitoringsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -9,11 +11,17 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.example.structuremonitoringsystem.Bluetooth.BluetoothConnection;
+import com.example.structuremonitoringsystem.RecyclerView.Adapter.BluetoothAdapter;
+import com.example.structuremonitoringsystem.RecyclerView.Item.BluetoothItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BluetoothTest extends AppCompatActivity {
 
     BluetoothConnection btConnection;
     TextView deviceList;
+    RecyclerView recyclerView;
     AppCompatButton cnBt;
 
     static int counter = 0;
@@ -25,25 +33,28 @@ public class BluetoothTest extends AppCompatActivity {
 
         deviceList = (TextView) findViewById(R.id.devList);
         cnBt = (AppCompatButton) findViewById(R.id.cnBtn);
+        recyclerView = (RecyclerView) findViewById(R.id.recView);
 
         btConnection = new BluetoothConnection();
 
+        String getDeviceNames = btConnection.getDeviceNameList(BluetoothTest.this);
+        String getDeviceMAC = btConnection.getDeviceMACList(BluetoothTest.this);
+//
+        String[] deviceName = getDeviceNames.split(",");
+        String[] deviceMAC = getDeviceMAC.split(",");
+
+        List<BluetoothItem> deviceItemList = new ArrayList<BluetoothItem>();
+
+        //converts the array to list
+        for(int x = 0; x < deviceName.length; x++){
+            deviceItemList.add(new BluetoothItem(deviceName[x], deviceMAC[x]));
+        }
 
         cnBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String getDeviceNames = btConnection.getDeviceNameList(BluetoothTest.this);
-                String getDeviceMAC = btConnection.getDeviceMACList(BluetoothTest.this);
-
-                String[] deviceName = getDeviceNames.split(",");
-                String[] deviceMAC = getDeviceMAC.split(",");
-
-                String devUID = btConnection.deviceUUID(BluetoothTest.this, deviceMAC[counter]);
-                String device = deviceName[counter] +" : "+ deviceMAC[counter] + " : " + devUID;
-
-                deviceList.setText(deviceName[counter] +" : "+ deviceMAC[counter]+ " : " + devUID);
-                Log.e("Bluetooth Name", device );
-                counter++;
+                recyclerView.setLayoutManager(new LinearLayoutManager(BluetoothTest.this));
+                recyclerView.setAdapter(new BluetoothAdapter(getApplicationContext(), deviceItemList));
             }
         });
 
