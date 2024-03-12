d9d9c7b66d2b6af5d5799f355fd98b5db80baf60
nyenyel <nieltayag10@gmail.com>
2024-03-12 22:50:55 +0800

    final activity that will be use for the system

---
+++ app/src/main/java/com/example/structuremonitoringsystem/Dashboard.java
@@ -0,0 +1,36 @@
+package com.example.structuremonitoringsystem;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.example.structuremonitoringsystem.OtherFunction.NavigationBar;
+import com.google.android.material.navigation.NavigationView;
+
+public class Dashboard extends AppCompatActivity {
+
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        navigationView = (NavigationView) findViewById(R.id.navbarView);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        NavigationBar navigationBar = new NavigationBar(drawerLayout, navigationView, toolbar);
+        navigationBar.setNavbar(this);
+    }
+}
