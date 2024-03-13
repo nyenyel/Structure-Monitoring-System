de8ff8bf9090cdb63d491c90fdf18adcf04f7f3f
nyenyel <nieltayag10@gmail.com>
2024-03-13 15:55:08 +0800

    created a new button for redirecting the activity for testing

--- app/src/main/java/com/example/structuremonitoringsystem/Testing/ObjectList.java
+++ app/src/main/java/com/example/structuremonitoringsystem/Testing/ObjectList.java
@@ -1,11 +1,15 @@
 package com.example.structuremonitoringsystem.Testing;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.structuremonitoringsystem.LocalDatabase.DatabaseHelper;
 import com.example.structuremonitoringsystem.R;
@@ -19,16 +23,20 @@ public class ObjectList extends AppCompatActivity {
 
     private RecyclerView recyclerView;
 
+    private AppCompatButton createObject;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.testing_object_list);
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        createObject = (AppCompatButton) findViewById(R.id.createObj);
 
         List<ObjectItem> objectItems= new ArrayList<ObjectItem>();
 
         DatabaseHelper databaseHelper = new DatabaseHelper(this);
+//        databaseHelper.changeDefaultObjectSize("3","2");
         Cursor objectList = databaseHelper.readAllData();
         try {
             if (objectList != null && objectList.moveToFirst()){
@@ -50,6 +58,13 @@ public class ObjectList extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(ObjectList.this));
         recyclerView.setAdapter(new ObjectAdapter(getApplicationContext(), objectItems));
 
+        createObject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ObjectList.this, CreateObject.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
