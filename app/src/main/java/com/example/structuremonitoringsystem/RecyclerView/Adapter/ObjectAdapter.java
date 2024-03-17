2199c0f895695abe905b6f3e51f721e20250a2b3
nyenyel <nieltayag10@gmail.com>
2024-03-18 04:08:16 +0800

    adapter is now used for setting default templates

--- app/src/main/java/com/example/structuremonitoringsystem/RecyclerView/Adapter/ObjectAdapter.java
+++ app/src/main/java/com/example/structuremonitoringsystem/RecyclerView/Adapter/ObjectAdapter.java
@@ -1,15 +1,23 @@
 package com.example.structuremonitoringsystem.RecyclerView.Adapter;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.structuremonitoringsystem.LocalDatabase.DatabaseDefaultSettings;
+import com.example.structuremonitoringsystem.LocalDatabase.DatabaseObjectSize;
 import com.example.structuremonitoringsystem.R;
-import com.example.structuremonitoringsystem.RecyclerView.Item.ObjectItem;
+import com.example.structuremonitoringsystem.Item.ObjectItem;
 import com.example.structuremonitoringsystem.RecyclerView.ViewHolder.ObjectViewHolder;
+import com.example.structuremonitoringsystem.Settings;
 
 import java.util.List;
 
@@ -17,6 +25,7 @@ public class ObjectAdapter extends RecyclerView.Adapter<ObjectViewHolder> {
 
     Context context;
     List<ObjectItem> items;
+    DatabaseObjectSize objectSize;
     public ObjectAdapter(Context context, List<ObjectItem> items) {
         this.context = context;
         this.items = items;
@@ -30,12 +39,13 @@ public class ObjectAdapter extends RecyclerView.Adapter<ObjectViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ObjectViewHolder holder, int position) {
+        objectSize = new DatabaseObjectSize(context);
+
         String objectId = items.get(position).getId();
         String objectTempName = items.get(position).getTempName();
         String objectHeight = items.get(position).getHeight();
         String objectWidth = items.get(position).getWidth();
         String objectThickness = items.get(position).getThickness();
-
 
         holder.tempName.setText(objectTempName);
         holder.height.setText(String.valueOf(Float.parseFloat(objectHeight)/10));
@@ -48,6 +58,26 @@ public class ObjectAdapter extends RecyclerView.Adapter<ObjectViewHolder> {
                 Float.parseFloat(objectHeight),
                 Float.parseFloat(objectThickness));
         holder.openGLView.rotateObject(45,45,120,-1,2f);
+        holder.templateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, Settings.class);
+                String oldID = "";
+                Cursor cursor = objectSize.getDefaultObjectSize();
+                try {
+                    if (cursor != null && cursor.moveToFirst()){
+                        do {
+                            oldID = cursor.getString(cursor.getColumnIndexOrThrow("_id"));
+                        }while (cursor.moveToNext());
+                    }
+                }finally {
+                    if (cursor != null){cursor.close();}
+                }
+                
+                objectSize.changeDefaultObjectSize(oldID, objectId);
+                startActivity(context, intent, null);
+            }
+        });
     }
 
     @Override
