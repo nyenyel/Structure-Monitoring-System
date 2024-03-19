32e7325218a15f982421df2affed0c34ef1cd26d
nyenyel <nieltayag@gmail.com>
2024-03-19 15:35:20 +0800

    new aactivity for list of sensor inside the device selected

---
+++ app/src/main/java/com/example/structuremonitoringsystem/ListOfSensorPerDevice.java
@@ -0,0 +1,39 @@
+package com.example.structuremonitoringsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.example.structuremonitoringsystem.OtherFunction.NavigationBar;
+import com.example.structuremonitoringsystem.RecyclerView.ShowItemList;
+import com.google.android.material.navigation.NavigationView;
+
+public class ListOfSensorPerDevice extends AppCompatActivity {
+
+    NavigationView navigationView;
+    RecyclerView recyclerView;
+    ShowItemList showItemList;
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_select_default_template);
+
+        navigationView = (NavigationView) findViewById(R.id.navbarView);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        setSupportActionBar(toolbar);
+        NavigationBar navigationBar = new NavigationBar(drawerLayout, navigationView, toolbar);
+        navigationBar.setNavbar(this);
+
+
+    }
+}
