dda0294eeaa72168846f638feb49a898d21456ad
nyenyel <154278884+nyenyel@users.noreply.github.com>
2024-11-24 16:54:19 +0800

    new feat

---
+++ database/migrations/2024_11_24_084511_add_cor_medcert_psa.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table->string('cor')->nullable()->after('student_no');
+            $table->string('psa')->nullable()->after('student_no');
+            $table->string('med_cert')->nullable()->after('student_no');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('players', function (Blueprint $table) {
+            //
+        });
+    }
+};
