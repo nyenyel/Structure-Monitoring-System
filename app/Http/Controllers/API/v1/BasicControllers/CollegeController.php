ee7765bf1890f8c10ffaeec547e8c8b8a6351271
nyenyel <154278884+nyenyel@users.noreply.github.com>
2025-02-27 06:57:03 +0800

    rev

--- app/Http/Controllers/API/v1/BasicControllers/CollegeController.php
+++ app/Http/Controllers/API/v1/BasicControllers/CollegeController.php
@@ -70,7 +70,11 @@ class CollegeController extends Controller
     {
         $banner = Banner::where('is_default',  true)->first();
         
-        return CollegeResource::make($college->load(['team' => function ($q) use ($banner) {$q->where('banner_id', $banner->id);}]));
+        return CollegeResource::make($college->load([
+            'team' => function ($q) use ($banner) {
+                $q->where('banner', $banner->id);
+            }
+        ]));
 
     }
 
