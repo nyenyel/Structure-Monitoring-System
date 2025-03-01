3cf9c1a50c885751b92767a2b7ec9535f01e6bcc
nyenyel <154278884+nyenyel@users.noreply.github.com>
2025-03-02 07:55:18 +0800

    rev

--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,7 @@ Route::prefix('v1')->group( function () {
         Route::put('search', [SearchController::class, 'search']);
 
         Route::post('add-checklist', [ChecklistController::class, 'addToChecklist']);
-        Route::get('get-checklist', [ChecklistController::class, 'search']);
+        Route::get('get-checklist', [ChecklistController::class, 'getChecklist']);
         Route::delete('remove-checklist/{checklist}', [ChecklistController::class, 'removeChecklist']);
 
     });
