43154e257c2fa5b737ba46519287004dbaf7f1e9
nyenyel <154278884+nyenyel@users.noreply.github.com>
2025-04-04 06:56:48 +0800

    rev

--- app/Http/Controllers/API/v1/BatchScheduleController.php
+++ app/Http/Controllers/API/v1/BatchScheduleController.php
@@ -260,6 +260,16 @@ class BatchScheduleController extends Controller
             return json_encode(['error' => $e->getMessage()]);
         }
     }
+    
+    public function scheduleMatchForPointingSystem(){
+        
+
+        return response()->json([
+            'mesage'=> "data added",
+            'code' => 200,
+            'data' => []
+        ]);
+    }
     private function calculateRounds($numberOfTeams) {
         if ($numberOfTeams < 2) {
             return 0; // No rounds needed if fewer than 2 teams
@@ -267,200 +277,24 @@ class BatchScheduleController extends Controller
         // Calculate the number of rounds needed
         return ceil(log($numberOfTeams, 2));
     }
+    
+    public function pointingSystem(Request $request){
+        $teams = $request->team;
+        $combinations = $this->getCombinations($teams, 2);
+        return response()->json(["data" => $combinations]);
+    }
+
+    private function getCombinations($array, $length) {
+        $result = [];
+        $count = count($array);
+    
+        for ($i = 0; $i < $count; $i++) {
+            for ($j = $i + 1; $j < $count; $j++) {
+                $result[] = [$array[$i], $array[$j]];
+            }
+        }
+    
+        return $result;
+    }
 }
 
-
-// <?php
-
-// namespace App\Http\Controllers\api\v1;
-
-// use App\Http\Controllers\Controller;
-// use App\Http\Resources\ScheduleResource;
-// use App\Models\Sports\Schedule;
-// use Exception;
-// use Illuminate\Http\Request;
-
-// class BatchScheduleController extends Controller
-// {
-//     public function batchSchedule(Request $request)
-//     {
-//         try {
-//             $predictedNumOfMatch = 0;
-//             $numOfTeams = 0;
-//             if($request->sports['num_of_teams'] == 3){
-
-//             }
-//             $predictedNumOfMatch = $request->sports['num_of_teams'] - 1;
-
-
-//             $numberOfRounds = $this->calculateRounds($request->sports['num_of_teams']);
-//             $tempNextMatchID = [];
-//             $counter = 1;
-//             $index = 0;
-//             $anotherCounter = 0;
-//             $isNotDone = true;
-//             $numOfMatch = 1;
-//             $temp = 1;
-//             $teams = $request->team;
-//             $template = [
-//                 'date' => null,
-//                 'time' => null,
-//                 'match_no' => '',
-//                 'match_round' => '',
-//                 'next_match_id' => null,
-//                 'first_team_id' => null,
-//                 'second_team_id' => null,
-//                 'winner_team_id' => null,
-//                 'sports_id' => $request->sports['id'],
-//                 'lib_game_statuses_id' => 1,
-//             ];
-    
-//             $newData = $template;
-    
-//             $result = ['message' => 'success'];
-    
-//             for ($x = $predictedNumOfMatch; $x > 0 ; $x--) {
-//                 $countInTwosMain = $counter % 2;
-
-//                 if ($x <= $request->sports['num_of_teams']/2) {
-
-//                     if($counter == 4 ){
-//                         $hold = $tempNextMatchID[1];
-//                         $tempNextMatchID[1] = $tempNextMatchID[2];
-//                         $tempNextMatchID[2] = $hold;
-//                     }
-
-//                     // foreach ($teams as $data) {
-//                     //     $countInTwos = $temp % 2;
-//                     //     if ($countInTwos == 1) {
-//                     //         $newData['first_team_id'] = $data;
-//                     //     } else {
-//                     //         $newData['second_team_id'] = $data;
-//                     //         $newData['match_no'] = $numOfMatch;
-//                     //         $newData['match_round'] = "Round " . $numberOfRounds;
-//                     //         $newData['next_match_id'] = $tempNextMatchID[$index];
-//                     //         $numOfMatch += 1;
-//                     //         Schedule::create($newData);
-//                     //         $newData = $template; // Reset to template
-//                     //         $anotherCounter++;
-//                     //         if($anotherCounter == 2){
-//                     //             $anotherCounter = 0;
-//                     //             $index++;
-//                     //         }
-//                     //     }
-//                     //     $temp++;
-//                     // }
-                    
-//                     foreach ($teams as $data) {
-//                         $countInTwos = $temp % 2;
-                        
-//                         // Check if we're at the last team and it's an odd number of teams
-//                         if ($request->sports['num_of_teams'] % 2 == 1 && $temp == $request->sports['num_of_teams']) {
-//                             // Bypass the last team to the next round
-//                             $schedule = Schedule::where('id', $tempNextMatchID[$index])->first();
-//                             $newData['first_team_id'] = $data;
-//                             $newData['second_team_id'] = null; // No second team
-//                             $newData['match_no'] = $schedule->match_no;
-//                             $newData['match_round'] = $schedule->match_round;
-//                             $newData['next_match_id'] = $schedule->next_match_id;
-//                             $schedule->update($newData);
-//                             return json_encode($schedule);
-//                         } else {
-//                             // Pair the teams normally
-//                             if ($countInTwos == 1) {
-//                                 $newData['first_team_id'] = $data;
-//                             } else {
-//                                 $newData['second_team_id'] = $data;
-//                                 $newData['match_no'] = $numOfMatch;
-//                                 $newData['match_round'] = "Round " . $numberOfRounds;
-//                                 $newData['next_match_id'] = $tempNextMatchID[$index];
-//                                 $numOfMatch += 1;
-//                                 Schedule::create($newData);  // Save match pairing
-//                                 $newData = $template; // Reset to template
-//                                 $anotherCounter++;
-//                                 if ($anotherCounter == 2) {
-//                                     $anotherCounter = 0;
-//                                     $index++;
-//                                 }
-//                             }
-//                         }
-//                         $temp++;
-//                     }
-//                     return json_encode($teams);
-//                 } else {
-//                     if($counter == 1){
-//                         $newData['match_round'] = "Round " . $numberOfRounds;
-//                         $newData['match_no'] = $x;
-//                         $schedule = Schedule::create($newData);
-//                         $tempNextMatchID[] = $schedule->id;
-//                         $numberOfRounds--;
-//                     } else if($counter > 1 && $counter < 4){
-//                         $newData['match_no'] = $x;
-//                         $newData['match_round'] = "Round " . $numberOfRounds;
-//                         $newData['next_match_id'] = $tempNextMatchID[$index];
-//                         $schedule = Schedule::create($newData);
-//                         $tempNextMatchID[] = $schedule->id;
-//                         $hold = 0;
-
-//                     } else{
-//                         if($counter == 4){
-//                             $numberOfRounds--;
-//                         }
-//                         $newData['match_no'] = $x;
-//                         $newData['match_round'] = "Round " . $numberOfRounds;
-//                         $newData['next_match_id'] = $tempNextMatchID[$index];
-//                         $schedule = Schedule::create($newData);
-//                         $tempNextMatchID[] = $schedule->id;
-
-//                         $hold = 0;
-//                         if($counter == 7 ){
-//                             $hold = $tempNextMatchID[3];
-//                             $tempNextMatchID[3] = $tempNextMatchID[6];
-//                             $tempNextMatchID[6] = $hold;
-
-//                             $hold = $tempNextMatchID[4];
-//                             $tempNextMatchID[4] = $tempNextMatchID[5];
-//                             $tempNextMatchID[5] = $hold;
-//                             $numberOfRounds--;
-//                             // return json_encode($tempNextMatchID);
-//                         }
-//                     }
-//                 }
-//                 if ($countInTwosMain == 1 && $counter != 1) {
-//                     $index++;
-//                 }
-
-//                 $newData = $template; // Reset to template
-//                 $counter++;
-//             }
-    
-//             return json_encode($result);
-    
-//         } catch (Exception $e) {
-//             return json_encode(['error' => $e->getMessage()]);
-//         }
-//     }
-
-//     public function scheduledMatch($sportId){
-//         try{
-
-//             $records = Schedule::whereNotNull(['date','time'], 'and' )
-//                             ->whereNull(['winner_team_id'])
-//                             ->where('sports_id', $sportId)
-//                             ->get();
-//             $records->load(['firstTeam', 'secondTeam', 'winningTeam', 'sports', 'gameStatus']);
-
-//             return ScheduleResource::collection($records);
-
-//         } catch (Exception $e) {
-//             return json_encode(['error' => $e->getMessage()]);
-//         }
-//     }
-//     private function calculateRounds($numberOfTeams) {
-//         if ($numberOfTeams < 2) {
-//             return 0; // No rounds needed if fewer than 2 teams
-//         }
-//         // Calculate the number of rounds needed
-//         return ceil(log($numberOfTeams, 2));
-//     }
-// }
