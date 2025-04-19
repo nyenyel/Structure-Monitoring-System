92064a07d1b9164ff6ba5931d510a5572358fb9c
nyenyel <154278884+nyenyel@users.noreply.github.com>
2025-04-20 05:21:24 +0800

    rev

--- routes/api.php
+++ routes/api.php
@@ -41,7 +41,7 @@ Route::prefix('v1')->group( function () {
         Route::post('add-checklist', [ChecklistController::class, 'addToChecklist']);
         Route::get('get-checklist', [ChecklistController::class, 'getChecklist'])->middleware('auth:sanctum');
         Route::delete('remove-checklist/{checklist}', [ChecklistController::class, 'removeChecklist']);
-        Route::post('compute-points/{sport}', [BatchScheduleController::class, 'computePoints']);
+        Route::get('compute-points/{sport}', [BatchScheduleController::class, 'computePoints']);
 
     });
     Route::prefix('compute')->group(function(){
