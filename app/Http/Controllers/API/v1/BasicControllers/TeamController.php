8605692ddeb0b0d34a86b7b0c989ee909f6c71a4
nyenyel <154278884+nyenyel@users.noreply.github.com>
2025-04-29 09:35:04 +0800

    rev

--- app/Http/Controllers/API/v1/BasicControllers/TeamController.php
+++ app/Http/Controllers/API/v1/BasicControllers/TeamController.php
@@ -55,11 +55,12 @@ class TeamController extends Controller
         
         // Create a new BasicInformation record
         $basicInformation = BasicInformation::create($validated['info']);
-        $secBasicInformation = BasicInformation::create($validated['sec_info']);
+
+        $secBasicInformation = $validated['sec_info'] ? BasicInformation::create($validated['sec_info']) : null;
 
         // Add the coach_id to the team data
         $validated['team']['coach_id'] = $basicInformation->id;
-        $validated['team']['sec_coach_id'] = $secBasicInformation->id;
+        $validated['team']['sec_coach_id'] = $secBasicInformation->id ? $secBasicInformation->id : null;
         $validated['team']['banner'] = $banner->id;
 
         // Create a new Team record
