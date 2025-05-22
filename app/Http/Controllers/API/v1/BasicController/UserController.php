47ee2a7d9fff9e9a67af8fd5b71924ef05b281c0
nyenyel <154278884+nyenyel@users.noreply.github.com>
2025-05-23 00:52:07 +0800

    evr

--- app/Http/Controllers/API/v1/BasicController/UserController.php
+++ app/Http/Controllers/API/v1/BasicController/UserController.php
@@ -22,13 +22,13 @@ class UserController extends Controller
         
         if ($user->college == null){
             $data = User::all();
-            $data->load(['basicInformation', 'role', 'college', 'team']);
+            $data->load(['basicInformation', 'role', 'college', 'team', 'team.sports']);
         } else {
             $data = User::where('college_id', $user->college->id)->get();
-            $data->load(['basicInformation', 'role', 'college', 'team']);
+            $data->load(['basicInformation', 'role', 'college', 'team', 'team.sports']);
         }
         
-        return UserRegistrationResource::collection($data);
+        return UserRegistrationResource::collection($data);/*  */
     }
 
     /**
